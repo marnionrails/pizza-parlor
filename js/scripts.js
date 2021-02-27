c4e7dd0d11c0d739b84aa5408a553ee0effdea1c
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-02-27 16:14:01 -0500

    add checkbox choices for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,17 @@ function Pizza() {
   this.size = 0;
 }
 
-Pizza.prototype.cost {
+Pizza.prototype.cost(function() {
   
-}
+});
+
+//4 dollars per slice, .5 cents per topping
+//choices of toppings: cheese, pepperoni, mushrooms, eggplant
+
+$(document).ready(function() {
+  $("form#pizza").submit(function(event) {
+    event.preventDefault();
+    
+
+  });
+});
